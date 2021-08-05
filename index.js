69a363f2b9c66c3932b54f6404eea86a21325e9c
tpsalo <tpsalo@utu.fi>
2021-08-05 20:23:40 +0300

    Tehtävä 3.20 tehty

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
   if (error.name === 'ValidationError') {
-    console.log("error.name: ", error.name)
+    console.log("error: ", error.message)
     return response.status(400).json({ error: error.message })
   } 
   
--- models/person.js
+++ models/person.js
@@ -12,8 +12,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
     })
 
 const personSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    number: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true, minlength: 3 },
+    number: { type: String, required: true, unique: true, minlength: 8 },
     id: { type: String }
 })
 
